7d5504b474fdba68577e1b3a1142663a1973ed69
Parth Jariwala <parth.jariwala13@gmail.com>
2025-11-08 21:55:22 +0530

    initial commit

---
+++ src/test/java/codes/parth/spring_webflux_mongo/SpringWebfluxMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.parth.spring_webflux_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
